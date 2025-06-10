84df07047f1225c4f7563367b7e73dbb75b0fab4
Kacper <kapelek82@outlook.com>
2025-06-10 18:04:32 +0100

    Start of large overhaul to Projects section as at the top of the page will be my final year university project and underneath, a two column display for full-stack projects

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,9 +1,19 @@
 function ProjectCard({ title, description, image, link }) {
   return (
-    <div style={{ border: '1px solid #ddd', borderRadius: '10px', padding: '1rem', marginBottom: '1rem' }}>
+    <div 
+      style={{
+        border: '1px solid #ddd', 
+        borderRadius: '10px', 
+        padding: '1rem', 
+        marginBottom: '1rem', 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'center'
+      }}
+    >
       <h3 style={{color:'grey'}}>{title}</h3>
       <p>{description}</p>
-      {image && <img src={image} alt={`${title || 'Project'} screenshot`} style={{ maxWidth: '100%' }} />} 
+      {image && <img src={image} alt={`${title || 'Project'} screenshot`} style={{ display:'flex', maxWidth: '50%' }} />} 
       {/*&& is an "if provided" statement if image variable is missing 
       maxWidth best practice for responsive design 
       || is a logical OR for error handling if Project title is missing*/}
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,4 +1,9 @@
 import ProjectCard from '../Components/ProjectCard';
+import homeImg from '../images/websiteHome.png';
+import eigenvalues from '../images/eigenvalues.png';
+import mainResults from '../images/mainResults.png';
+
+
 
 function Projects() {
   const projects = [
@@ -6,18 +11,37 @@ function Projects() {
       title: 'Portfolio Website (HTML/CSS)',
       description: 'My first hand-coded portfolio website built with HTML, CSS, and vanilla JS.',
       link: 'https://github.com/KacperProg/New_Portfolio_Website',
+      image: homeImg
     },
     {
       title: 'New React Portfolio',
-      description: 'This current site, rebuilt using React and Vite.',
+      description: 'This current site, constructed using React and Vite.',
       link: 'https://github.com/KacperProg/React-Portfolio-Site',
     },
-  ];
+  ]
 
   return (
     <div>
-      <h1>My Projects</h1> 
-      
+      <div style={{
+        border: '1px solid #ddd', 
+        borderRadius: '10px', 
+        padding: '1rem', 
+        marginBottom: '1rem', 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'center'}}
+        >
+        <h1>My Projects</h1> 
+        {/* Section to describe my final year project. Different to other ProjectCards as this will have double width
+        and be highlighted at the top of the page. Remaining projects will display below. */}
+        <h2>My Final Year Physics project</h2>
+        <p>This project was part of my dissertation. I used advanced MATLAB code to analyse the topology of T-Cells.</p>
+        <div > 
+          {/* Div in the future use as container to display images in a row/in line in which case the style from img will be removed*/}
+          <img src={eigenvalues} style={{ display:'inline-flex', maxWidth: '50%' }}></img>
+          <img src={mainResults} style={{ display:'inline-flex', maxWidth: '50%' }}></img>
+        </div>
+      </div>
       {/* mapping (for loop) allowing me to expand how many projects 
       I list in this section based on contents of projects variable */}
 
