aa26e988c9515f88e73e6dbe10374f874edcfaa3
Kacper <kapelek82@outlook.com>
2025-06-20 23:56:36 +0100

    Moved all styling from Home jsx to Home css for more clearer code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,20 @@
 import { links } from '../data/links';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import "./Home.css";
 
 // styling added in elements for now and later moved to CSS. Easier for structuring CSS file later
 
 function Home() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}> 
+    <div className='main-container'> 
       <h1>Hi, I'm Kacper Nowak 👋</h1>
       <p>I'm 24 and I'm working on React right now!</p>
-      <div style={{ marginTop: '2rem' }}>
+      <div className='buttons-container'>
         <a
           href={links.linkedin}
           target="_blank" // Opens the link in a new tab
           rel="noopener noreferrer" // Security protocol, best practice 
-          style={{
-            display: 'inline-flex',
-            alignItems: 'center',
-            gap: '0.5rem',
-            padding: '0.5rem 1rem',
-            backgroundColor: '#333', 
-            color: '#fff',
-            borderRadius: '5px',
-          }} 
+          className="link-button"
         >
           <FaLinkedin/>
           LinkedIn
@@ -31,24 +24,18 @@ function Home() {
           href={links.github}
           target="_blank" // Opens the link in a new tab
           rel="noopener noreferrer" // Security protocol, best practice 
-          style={{
-            display: 'inline-flex',
-            alignItems: 'center',
-            gap: '0.5rem',
-            padding: '0.5rem 1rem',
-            backgroundColor: '#333', 
-            color: '#fff',
-            borderRadius: '5px',
-          }} 
+          className="link-button" 
         >
           <FaGithub/>
           GitHub
         </a>
       </div>
-      <h2>About me</h2>
-      <h3>I'm currently living in abc up-skilling by re-learning React</h3>
-      <p style={{paddingTop:"500px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aliquid perspiciatis labore, sed ea inventore provident esse sequi voluptatibus mollitia officia, pariatur sit explicabo amet quos, iusto quis dolor. Aliquam?</p>
-    {/* Text added to allow scrolling down on page for putting together size changing Navbar feature */}
+      <article>
+        <h2>About me</h2>
+        <h3>I'm currently living in abc up-skilling by re-learning React</h3>
+        <p style={{paddingTop:"500px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aliquid perspiciatis labore, sed ea inventore provident esse sequi voluptatibus mollitia officia, pariatur sit explicabo amet quos, iusto quis dolor. Aliquam?</p>
+        {/* Text added to allow scrolling down on page for putting together size changing Navbar feature */}
+      </article>
     </div>
   );
 }
