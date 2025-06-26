6093341cebd63fb3a60c52735070a36a3cdde2d8
Kacper <kapelek82@outlook.com>
2025-06-26 21:51:13 +0100

    Begun adding CSS to skills page

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,54 +1,59 @@
 import {progLang, spokenLang, tools, softSkills, certification} from '../data/skills';
+import "./Skills.css";
 
 // Skills mapped from dictionaries and lists/arrays created in skills.js for easier updating methodology
 
 function Skills() {
     return(
-        <div style={{ padding: `1rem`}}>
-            <h1>Skills</h1>
-            <p>Below find a list of all skills and relevant qualifications</p>
-            <h2>Programming</h2>
-            <ul>                           {/* fake error*/}
-                {progLang.map(({ name, level, logo: Icon }) => ( 
-                    <div key={name}> {/* add styling/classname here*/}
-                        <Icon size={28}/> {/* here*/}
-                        <div>
-                            <p>{name}</p>
-                            <span >{level}</span> {/* and here to make logos appear next to text*/}
+        <div style={{ padding: "2rem"}}>
+            <div className='title'>
+                <h1>A Display page of my skills and qualifications!</h1>
+            </div>
+                <p>Below find a list of all skills and relevant qualifications</p>
+            <div>
+                <h2>Programming</h2>
+                <ul>                           {/* fake error*/}
+                    {progLang.map(({ name, level, logo: Icon }) => ( 
+                        <div key={name}> {/* add styling/classname here*/}
+                            <Icon size={28}/> {/* here*/}
+                            <div>
+                                <p>{name}</p>
+                                <span >{level}</span> {/* and here to make logos appear next to text*/}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
 
-                {/* Dictionary mapping logic immediately above*/}
+                    {/* Dictionary mapping logic immediately above*/}
 
-            </ul>
-            <h2>Languages</h2>
-            <ul>
-                {spokenLang.map((lang) => (
-                    <li key={lang.name}>{lang.name} | {lang.level}</li>
-                ))}
-            </ul>
-            <h2>Tools</h2>
-            <ul>
-                {tools.map((tool) => (
-                    <li key={tool}>{tool}</li>
-                ))}
+                </ul>
+                <h2>Languages</h2>
+                <ul>
+                    {spokenLang.map((lang) => (
+                        <li key={lang.name}>{lang.name} | {lang.level}</li>
+                    ))}
+                </ul>
+                <h2>Tools</h2>
+                <ul>
+                    {tools.map((tool) => (
+                        <li key={tool}>{tool}</li>
+                    ))}
 
-                {/* List/Array mapping logic immediately above*/}
+                    {/* List/Array mapping logic immediately above*/}
 
-            </ul>
-            <h2>Soft Skills</h2>
-            <ul>
-                {softSkills.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                ))}
-            </ul>
-            <h2>Certificates</h2>
-            <ul>
-                {certification.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                ))}
-            </ul>
+                </ul>
+                <h2>Soft Skills</h2>
+                <ul>
+                    {softSkills.map((skill) => (
+                        <li key={skill}>{skill}</li>
+                    ))}
+                </ul>
+                <h2>Certificates</h2>
+                <ul>
+                    {certification.map((skill) => (
+                        <li key={skill}>{skill}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
